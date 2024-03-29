94c504d8ff48f230d2f1925f7da7fab5119e62a7
pauladamadrian <adrian.adam@takeofflabs.com>
2024-03-29 22:53:06 +0200

    One more for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -43,6 +43,7 @@ RSpec.describe Post, type: :model do
   end
 
   it 'has numerical views' do
-
+    post = Post.new(title: 'Title', body: 'Body', user: current_user, views: 0)
+    expect(post.views).to be_a(Integer)
   end
 end
